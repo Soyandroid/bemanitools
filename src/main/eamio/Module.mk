27156f1b1a72535d1acb11a5b06d33936f3a7c9d
BtbN <btbn@btbn.de>
2019-03-19 22:01:36 +0100

    Import libnfc libs and headers

--- src/main/eamio/Module.mk
+++ src/main/eamio/Module.mk
@@ -1,6 +1,12 @@
-dlls            += eamio
-libs_eamio      := geninput util
-src_eamio       := \
+eamio_curdir     := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+dlls             += eamio
+libs_eamio       := geninput util
+abslibs_eamio_32 := $(eamio_curdir)/nfc/lib/i686/libnfc.a
+abslibs_eamio_64 := $(eamio_curdir)/nfc/lib/x86_64/libnfc.a
+ldflags_eamio    := -lwinscard
+cflags_eamio     := -I"$(eamio_curdir)" -DLIBNFC_STATIC
+src_eamio        := \
     eam-api.c \
     eam-impl.c \
     eam-s11n.c \
